53df63054ff232583c49d741fea6592e53572751
Bastian Wagner <bastian.wagner@softconcis.de>
2021-08-24 10:06:26 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertViewComponent } from './alert-view/alert-view.component';
+import { DisplayComponent } from './display/display.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { SimpleAlertViewComponent } from './simple-alert-view/simple-alert-view.component';
+import { TabComponent } from './tab/tab.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TimerComponent } from './timer/timer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertViewComponent,
+    DisplayComponent,
+    ProgressBarComponent,
+    SimpleAlertViewComponent,
+    TabComponent,
+    TabsComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
